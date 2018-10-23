31432cb0b7ac86b2c8695afe0a09f843b0a62ff9
gyl <15124577336@163.com>
2018-10-23 04:02:11 -0600

    call add function

--- multiplication/mul1.c
+++ multiplication/mul1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "add.h"
 #define MAXMINE 100
 
 char* multiplication(char* m,char* n );
@@ -22,95 +23,63 @@ char* multiplication(char* a,char* b){
 	int a_length=strlen(a);
 	int b_length=strlen(b);
 	int length=a_length+b_length;
-	char* result=(char*)malloc(length);
-	memset(result,0,length);
-	char* result_2=(char*)malloc(length);
-	memset(result_2,0,length);
+	char* result=(char*)malloc(length + 1);
+	memset(result,'0',length);
+	result[length] = '\0';
+	char* result_2=(char*)malloc(length + 1);
+	memset(result_2,'0',length);
+	result_2[length] = '\0';
 	
-	char* temp_long=NULL;
-	char* temp_short=NULL;
+	char* temp_long=a;
+	char* temp_short=b;
 
-
-	int max,min,i,j;
-	if(a_length>b_length){
-		max = a_length;
-		min = b_length;
-		temp_long = a;
-		temp_short = b;
-	}else{
-		max = b_length;
-		min = a_length;
-		temp_long = b;
-		temp_short = a;
-	}
-
-	int cb=0,ww=0;//cb is carry bit
-	/***********first***************/
-	 int t=0;
-    for(j=max-1;j>=0;j--){
-    	ww=(temp_short[min-1]-'0')*(temp_long[j]-'0')+cb;
-		result[t]=ww%10;
-		cb=ww/10;
-		t++;
-    }
-	if(cb!=0){
-		result[t]=cb;
-	}
-	printf("\n t1= %d\n",t);
-	for(t=0;t<length;t++)
-		printf("result[t]= %d ",result[t]);//above is right;
-
-
-    /**********************************/
-	cb=0;
-	for(i=min-2;i>=0;i--){
-		t=min-1-i;
-		for(j=max-1;j>=0;j--){
-			ww=(temp_short[i]-'0')*(temp_long[j]-'0')+cb;
+	
+	int i,j;
+	int cb=0,ww=0;
+	int t=length -1;
+	int count = 0;
+	for(i = b_length - 1; i >= 0; i--){ //
+		t = length - 1 - count;
+		memset(result_2,'0',length);
+		cb = 0; 
+		for(j = a_length - 1; j >= 0; j--){
+			ww=(a[j]-'0')*(b[i]-'0')+cb;
 			result_2[t]=ww%10;
 			cb=ww/10;
-			t++;
+			result_2[t] = result_2[t] + '0';
+			t--;
 		}
 		if(cb!=0){
-			result_2[t]=cb;
+			result_2[t]=cb + '0';
 		}
-		for(j=0;j<min-1-i;j++)
-			result_2[j]=0;
-		printf("\n\n");
-		for(j=0;j<length;j++)
-			printf("result_2[j]= %d ",result_2[j]);//
-		printf("\n");
-
-	    cb=0;
-		for(j=0;j<length;j++){
-			ww=result[j]+result_2[j]+cb;
-			result[j]=ww%10;
-			cb=ww/10;
-		}
-		printf("\n");
-
-		if(cb!=0){
-			result[j]=cb;
-			printf("r[t]=%d \n",result[j]);
-		}
-		for(j=0;j<length;j++){
-			printf("r1= %d ",result[j]);
-		}
+		char* temp_result = add(result_2,result);
+		memcpy(result,temp_result,strlen(temp_result));
+		free(temp_result);
+		count = count + 1;
 	}
+
+	printf("------ result = %s\n", result);
+
+    /**********************************/
 	t=0;
-	printf("\n\n");
-	char* mul = (char*)malloc(length);
-	memset(mul,'0',length);
-for(j=length-1;j>=0;j--){
-    if(result[j]==0){
-    	continue;
-    }
-	mul[t]=result[j]+'0';
-	printf(" mul= %c ",mul[t]);
-	t++;
-}
-mul[t]='\0';
-free(result_2);
-free(result);
-return mul;
+	int result_length = strlen(result);
+	char* mul = (char*)malloc(result_length+1);
+	memset(mul,'0',result_length);
+	int sig = 0;
+	for(j=0;j<result_length;j++){
+		if(sig == 0){
+			if(result[j]=='0'){
+				continue;
+			}
+			else
+				sig = 1;
+		}
+		mul[t]=result[j];
+		printf(" mul= %c ",mul[t]);
+		t++;
+	}
+	mul[t]='\0';
+	free(result_2);
+	free(result);
+	return mul;
 }
